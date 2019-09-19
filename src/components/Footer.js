8c367c1cd796f4d5546cbe9b3ff3beb9d2f14c94
77ccreed <kiur.jullis@gmail.com>
2019-09-19 10:12:43 +0300

    kontaktid

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -61,7 +61,7 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/contact">
+                      <Link className="navbar-item" to="/kontaktid">
                         Kontaktid
                       </Link>
                     </li>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -69,7 +69,7 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/blog">
                 Tehtud tööd
               </Link>
-              <Link className="navbar-item" to="/contact">
+              <Link className="navbar-item" to="/kontaktid">
                 Kontaktid
               </Link>
             </div>
