6bdd601518a69b143700388171f2c56582dd2cec
77ccreed <kiur.jullis@gmail.com>
2019-09-19 10:19:13 +0300

    kontaktid leht eesti keelseks

--- src/pages/kontaktid/index.js
+++ src/pages/kontaktid/index.js
@@ -39,11 +39,11 @@ export default class Index extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-              <h1>Contact</h1>
+              <h1>Kontaktid</h1>
               <form
                 name="contact"
                 method="post"
-                action="/contact/thanks/"
+                action="/kontaktid/saadetud/"
                 data-netlify="true"
                 data-netlify-honeypot="bot-field"
                 onSubmit={this.handleSubmit}
@@ -58,7 +58,7 @@ export default class Index extends React.Component {
                 </div>
                 <div className="field">
                   <label className="label" htmlFor={'name'}>
-                    Your name
+                    Nimi
                   </label>
                   <div className="control">
                     <input
@@ -73,7 +73,7 @@ export default class Index extends React.Component {
                 </div>
                 <div className="field">
                   <label className="label" htmlFor={'email'}>
-                    Email
+                    E-mail
                   </label>
                   <div className="control">
                     <input
@@ -88,7 +88,7 @@ export default class Index extends React.Component {
                 </div>
                 <div className="field">
                   <label className="label" htmlFor={'message'}>
-                    Message
+                    Sõnum
                   </label>
                   <div className="control">
                     <textarea
@@ -102,7 +102,7 @@ export default class Index extends React.Component {
                 </div>
                 <div className="field">
                   <button className="button is-link" type="submit">
-                    Send
+                    Saada
                   </button>
                 </div>
               </form>
