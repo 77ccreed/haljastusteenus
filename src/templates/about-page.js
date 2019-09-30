37c0bb0137780c018e81e256a6eacf88dcd51a7f
77ccreed <kiur.jullis@gmail.com>
2019-09-30 11:57:11 +0300

    helmet

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import Helmet from 'react-helmet'
 import Content, { HTMLContent } from '../components/Content'
 
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
